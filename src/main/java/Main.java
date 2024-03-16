2c0d354f5afaf3aa6ddf3c4356615954aa7f8045
younesmeskafe <younesmeskafe@younesmeskafes-MacBook-Pro.local>
2024-03-16 17:16:42 +0000

    return 200Ok

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,32 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
      ServerSocket serverSocket = null;
      Socket clientSocket = null;
+     final String HTTP_OK = String.format("%s\r\n\r\n", "HTTP/1.1 200 OK");
 
      try {
        serverSocket = new ServerSocket(4221);
        serverSocket.setReuseAddress(true);
-       clientSocket = serverSocket.accept(); // Wait for connection from client.
+       clientSocket = serverSocket.accept();
+       // handle upcomming connection
+         InputStream inputStream = clientSocket.getInputStream();
+         readData(inputStream);
+         clientSocket.getOutputStream().write(HTTP_OK.getBytes());
        System.out.println("accepted new connection");
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
   }
+
+  public static void readData(InputStream data) {
+      // read data
+  }
+
+
 }
